f08a40e4bb4edddffa7fb53f65721414e2b9ea69
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2022-04-29 12:28:13 +0200

    Update navigation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,8 +10,8 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 }
 const navigation = [
-  { name: "Dashboard", href: "/", current: true },
-  { name: "Projects", href: "/projects", current: false },
+  { name: "Dashboard", href: "/dashboard", current: true },
+  { name: "Projects", href: "/dashboard/projects", current: false },
   { name: "Providers", href: "#", current: false },
 ]
 const userNavigation = [
